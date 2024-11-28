8d7f2baaf5477f708a24aa920b5c48d6f30367d8
Jeremy_Braem <jeremy.braem@formation-industries-ca.fr>
2024-11-28 14:14:43 +0100

    client/server

--- client.py
+++ client.py
@@ -1,15 +1,11 @@
 import socket
 
-def request():
-    HOST = ''
-    PORT = 50007
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                conn.sendall(data)
+HOST = "127.0.0.1"
+PORT = 5007
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
